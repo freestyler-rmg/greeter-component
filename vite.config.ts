cab0557db49f2f86d125c38e7114ed6dcfe602b2
Roy Margasa <roy.margasa@gmail.com>
2025-04-14 17:11:24 +0700

    Update readme and add gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
+  base: '/simple-todos',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
